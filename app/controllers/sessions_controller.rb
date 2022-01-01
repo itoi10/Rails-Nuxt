014d17ae871bb45f42e1005080929cc874a6bd93
itict10 <itict10@gmail.com>
2022-01-02 03:37:27 +0900

    add login success

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       # ユーザーログイン後にユーザー情報のページにリダイレクトする
-
+      log_in user
+      redirect_to user # user_url(user)
     else
       # flashをflash.nowに置き換えると、レンダリングが終わっているページでフラッシュメッセージを表示することができる
       flash.now[:danger] = 'メールアドレスまたはパスワードが間違っています'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
 module SessionsHelper
+  # ブラウザ内の一時cookiesに暗号化済みのユーザーIDが作成される
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # 現在ログイン中のユーザーを返す（いる場合）
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
