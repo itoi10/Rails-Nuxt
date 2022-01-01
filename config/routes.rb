e97d5c963cd7ccc10267ff210954e73e34d5c534
itict10 <itict10@gmail.com>
2022-01-02 02:58:31 +0900

    add session controller, login page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,11 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
 
+  # セッション
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
   # GET     /users            | index
   # GET     /users/<:id>      | show
   # GET     /users/new        | new
