a250f3f213053f15fcb7094b866568292473c936
itict10 <itict10@gmail.com>
2022-01-09 19:20:10 +0900

    (nuxt) fetch users list

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,33 +1,70 @@
 <template>
   <div>
-    <button
-      type="button"
-      name="button"
-      @click="getMsg"
-    >
-      Call Rails API
-    </button>
-    <div
-      v-for="(msg, i) in msgs"
-      :key="i"
-    >
-      {{ msg }}
+    <h2>
+      Usersテーブルの取得
+    </h2>
+    <table border="1" v-if="users.length">
+      <thead>
+        <tr>
+          <th>id</th>
+          <th>name</th>
+          <th>email</th>
+          <th>created_at</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(user, i) in users"
+          :key="`user-${i}`"
+        >
+          <td>{{ user.id }}</td>
+          <td>{{ user.name }}</td>
+          <td>{{ user.email }}</td>
+          <td>{{ dateFormat(user.created_at) }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div v-else>
+      <button
+        type="button"
+        name="button"
+        @click="getUsers"
+      >
+        RailsAPIからUser一覧取得
+      </button>
     </div>
   </div>
 </template>
+
 
 <script>
 export default {
   data() {
     return {
-      msgs: []
+      users: []
     }
   },
+  // ページのコンポーネントがロードされる前に呼ばれる
+  // async asyncData ({ $axios }) {
+  // },
   methods: {
-    getMsg() {
-      this.$axios.$get('/api/v1/hello')
-        .then(res => this.msgs.push(res))
+    getUsers() {
+      this.$axios.$get('/api/v1/users')
+        .then(res => (this.users = res))
+    }
+  },
+  computed: {
+    // 日付フォーマット変換
+    dateFormat () {
+      return (date) => {
+        const dateTimeFormat = new Intl.DateTimeFormat(
+          'ja', { dateStyle: 'medium', timeStyle: 'short' }
+        )
+        return dateTimeFormat.format(new Date(date))
+      }
     }
   }
 }
 </script>
+
