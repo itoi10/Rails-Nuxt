1e8d3ab6f2661a5ac292e070a06fc6eb6e6d9295
itict10 <itict10@gmail.com>
2022-01-09 21:45:45 +0900

    setup @nuxtjs/i18n

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -43,7 +43,10 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios' // axios
+    '@nuxtjs/axios', // axios
+
+    // https://www.npmjs.com/package/@nuxtjs/i18n
+    '@nuxtjs/i18n',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
@@ -68,6 +71,21 @@ export default {
           error: 'FB8678',
           background: 'f6f6f4'
         }
+      }
+    }
+  },
+
+  i18n: {
+    locales: ['ja', 'en'],
+    defaultLocale: 'ja',
+    // Doc: https://kazupon.github.io/vue-i18n/api/#properties
+    vueI18n: {
+      fallbackLocale: 'ja',
+      // silentTranslationWarn: true,
+      silentFallbackWarn: true,
+      messages: {
+        ja: require('./locales/ja.json'),
+        en: require('./locales/en.json')
       }
     }
   },
