a9b8faf870d301eca5a615883735e5e5f5eb3100
itict10 <itict10@gmail.com>
2022-01-15 20:34:57 +0900

    add logout action

---
+++ nuxt/pages/logout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  layout: 'logout',
+  // トップページからのログアウト時にレイアウト変更されるように、
+  // ログアウトのレイアウトを表示させた後にリダイレクト実行
+  async middleware ({ store, redirect, from }) {
+    await store.dispatch('logout')
+    if (from.name !== 'index') { return redirect('/') }
+  },
+  beforeCreate () {
+    this.$router.replace('/')
+  }
+}
+</script>
